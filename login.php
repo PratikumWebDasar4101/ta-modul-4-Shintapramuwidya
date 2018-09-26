6397d49d592d3f5a2a34c87a88222c3d751ab9ff
Shintapramuwidya <42725343+Shintapramuwidya@users.noreply.github.com>
2018-09-26 19:57:43 +0700

    Update and rename proses_login.php to login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div style="background-color: skyblue;"><center>
+			<img src="1.jpg" width="50">
+			<font size="10" color="blue">SELAMAT DATANG DI BLOG INI</font>
+			<img src="1.jpg" width="50">
+		<hr>
+	</div></center>
+	
+	<form action="formawal.html" method="Post">
+	username: <input type="text" name="username">
+	<br>
+	password: <input type="password" name="password">
+	<br>
+	<input type="submit" name="login" value="login">
+	</form>
+</body>
+</html>
+
