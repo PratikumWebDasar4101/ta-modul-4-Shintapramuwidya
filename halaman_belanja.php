1e4f079d0dee8747acfe4226ffa15d077887b601
Shintapramuwidya <42725343+Shintapramuwidya@users.noreply.github.com>
2018-09-26 19:51:55 +0700

    Update halaman_belanja.php

--- halaman_belanja.php
+++ halaman_belanja.php
@@ -1,9 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php
+session_start();
+$hrgapngrman;
+$totalhrg;
+$tgl=date('d-m-Y');
 
-</body>
-</html>
+$pengiriman = $_POST["pengiriman"];
+$belanja 	= $_POST["belanja"];
+$alamat 	= $_POST["alamat"];	
+
+ echo "Tanggal Pembeliaan : $tgl <br><br>";
+ echo "Alamat : $alamat";
+ echo "<p>Barang yang dibeli: </p>";
+
+ foreach ($belanja as $i) {
+	echo "- $i <br />";
+}
+
+$ttlBiaya = 0;
+
+if ($pengiriman=="JNE") {
+	$hrgapngrman = 10000;
+}elseif ($pengiriman=="TiKi") {
+	$hrgapngrman = 12000;
+}elseif ($pengiriman=="J&T"){
+	$hrgapngrman = 8000;
+}
+
+for ($x = 0; $x < count($belanja); $x++) { 
+	if ($belanja[$x] == "TV") {
+		$harga = 2000000;
+	} elseif($belanja[$x] == "Mesin Cuci"){
+		$harga = 3000000;
+	} else {
+		$harga = 500000;
+	}
+
+	$ttlBiaya = $ttlBiaya + $harga;
+}
+error_reporting(0);
+echo "<br>Harga Pengiriman $pengiriman : Rp. ". number_format($hrgapngrman)."<br>";
+$ttlBiaya = $ttlBiaya + $hrgapngrman;
+$baris = count($_SESSION['daftarbelanja']);
+$_SESSION['daftarbelanja'][$baris] = array(
+	"belanja" => $belanja, "pengiriman" => $pengiriman, "alamat" => $alamat, "totalBiaya" => $ttlBiaya
+);
+$daftarbelanjaan = $_SESSION['daftarbelanja'];
+echo "<br>Total Harga = Rp." . number_format($daftarbelanjaan[0]['totalBiaya']);
+
+?>
+<br><br>
+<a href="logout.php">Logout</a>
